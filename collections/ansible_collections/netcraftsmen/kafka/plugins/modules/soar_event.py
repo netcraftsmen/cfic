737580cf7008d9d239ccf7ad0325cd3f5c6b0fea
Joel W. King <programmable.networks@gmail.com>
2023-05-03 14:50:17 -0400

    updated soar_event

--- collections/ansible_collections/netcraftsmen/kafka/plugins/modules/soar_event.py
+++ collections/ansible_collections/netcraftsmen/kafka/plugins/modules/soar_event.py
@@ -10,64 +10,64 @@ DOCUMENTATION = '''
 ---
 module: cucm.admin
 
-short_description: Administration of Cisco Unified Communications Manager using AXL
-version_added: "2.13.7"
+short_description: Create Splunk SOAR events
+version_added: "2.14.5"
 
 description:
-    - "Ansible module to administration of the Call Manager via the AXL library"
+    - "Ansible module to create Splunk SOAR events (containers and artifacts)"
 
 options:
-    arguments:
+    authtoken:
       description:
-        - The arguments passed to the method
-      required: false
-      type: dict
-
-    hostname:
-      description:
-        - The hostname or IP address of the CUCM
+        - The API Bearer Token, "ph-auth-token" for the 'automation' user
       required: true
       type: str
 
-    username: 
+    server:
       description:
-        - The username used to logon the CUCM
-      default: administrator
-      required: false
-      type: str
-
-    password:
-      description:
-        - The password associated with the username
+        - The host IP or FQDN of the Splunk SOAR instance
       required: true
       type: str
 
-  
+    TODO
+
 author:
     - Joel W. King (@joelwking)
 '''
 EXAMPLES = '''
 
----
-- name: Manage Users
-  hosts: localhost
+    - name: Add artifact to new container
+      netcraftsmen.kafka.soar_event:
+        server: ec2-54-164-159-148.compute-1.amazonaws.com
+        authtoken: "1GvdkA220zFlbJMjISredactedm9QANjCg0k="
+        # container_id: 3
+        container:
+          name: "Voltaire" 
+          description: "French Enlightenment writer, historian, and philosopher."
+        artifact:
+          name: "cfic"
+          source_data_identifier: "IR_3458575"
+        cef:
+          sourceAddress: 192.0.2.1
+          sourcePort: 6553 
+          sourceUserId: 'voltaire@example.net'
+        metadata:
+          Influenced_by: "John Locke, William Shakespeare, Isaac Newton"
+          Born: "November 21, 1694"
+          quote: "Judge of a man by his questions rather than by his answers."
 
-  vars:
-    cucm_anchor: &cucm_opts
-      hostname: '10.10.20.1'
-      username: administrator
-      password: 'ciscopsdt'
-      version: '12.5'
-
-  tasks:
-    - name: Add User
-      netcraftsmen.cucm.admin:
-        <<: *cucm_opts
-        method: add_user
-        arguments:
-          userid: 'jking@netcraftsmen.com' 
-          lastName: 'King' 
-          firstName: 'Joel'
+    - name: Add artifact to new container
+      netcraftsmen.kafka.soar_event:
+        server: ec2-54-164-159-148.compute-1.amazonaws.com
+        authtoken: "1GvdkA220zFlbJMjISredactedm9QANjCg0k="
+        container_id: 3
+        artifact:
+          name: "cfic"
+          source_data_identifier: "IR_3458575"
+        cef:
+          sourceMacAddress: “00:0D:60:AF:1B:61”
+          msg: "An arbitrary message giving more details about the event."
+          
 
 '''
 from ansible.module_utils.basic import AnsibleModule
@@ -80,23 +80,29 @@ except ImportError:
 
 def container():
     """
+      TODO, for now, assume we have an existing container.
     """
     return
 
-def artifact(_object, container_id, cef, meta_data, art_i_fact):
+def artifact(_object, container_id, cef, metadata, artifact):
     """
+      Add the artifact to the container (event)
     """
-    artifact_id = _object.add_artifact(container_id, cef, meta_data, **art_i_fact)
-    return (dict(artifact_id=artifact_id, status_code=p.status_code)
+    try:
+        artifact_id = _object.add_artifact(container_id, cef, metadata, **artifact)
+    except:
+        return dict(status_code=500)
+    
+    return dict(artifact_id=artifact_id, status_code=_object.status_code)
 
 def main():
     module = AnsibleModule(
         argument_spec=dict(
             server=dict(required=True),
             authtoken=dict(required=True, no_log=True),
-            container_id=dict(required=True),
+            container_id=dict(required=False),
             container=dict(required=False, type=dict, default=dict())
-            artifact=dict(required=False, type=dict, default=dict()),
+            artifact=dict(required=False, type=dict, default=dict(name='default')),
             cef=dict(required=False, type='dict', default=dict()),
             metadata=dict(required=False, type='dict', default=dict()),
         ),
@@ -111,12 +117,14 @@ def main():
     container_id = module.params.get('container_id')
     container = module.params.get('container')
 
+    if container and container_id:
+       module.fail_json(msg="Specify either container ID or container, not both.")
+
     try:
         p = ingest.PhantomIngest(f"https://{server}", f"{authtoken}")
     except Exception as e:
         module.fail_json(msg=f'Exception: {e}')
 
-    container_id = module.params.get('container_id')  # TODO for now, required
     if container:
         container_id = container(container)  # TODO Create container if not specified.
     
@@ -124,12 +132,13 @@ def main():
     metadata = module.params.get('metadata')
     artifact = module.params.get('artifact')
 
-    result = artifact()
+    result = artifact(artifact(p, container_id, cef, metadata, artifact))
     result['changed'] = False
     if result.get('status_code') in (200,):  # TODO is string or integer?
         result['changed'] = True
 
     module.exit_json(**result)
+
 
 if __name__ == "__main__":
     main()
